d0e75ffc48645bc4b74f82a614c3ec9c59c39690
Andrew Kramer <ajkramer12@gmail.com>
2015-09-19 13:12:08 -0400

    Fixed input validation notices; Added numbers and symbols

--- logic.php
+++ logic.php
@@ -17,37 +17,39 @@
 	//Validate User Submitted Variables
 	//  Assume default value unless the user submitted variable validates
 	$wordCount = $defaultWordCount;
-	if ($_GET["wordCount"] > 0 && $_GET["wordCount"] <= $maxWordCount) {
-		$wordCount = $GET["wordCount"];
-	}
-	
 	$separator = $defaultSeparator;
-	if (in_array($_GET["separator"], $possibleSeparators)) {
-		$separator = $_GET["separator"];
-	}
-	
 	
 	$numbers = $defaultNumbers;
-	if ($_GET["numbers"] > 0 && $_GET["numbers"] <= $maxNumbers) {
-		$numbers = $_GET["numbers"];
-	}
-	
 	$numberLocationRandom = $defaultNumberLocationRandom;
-	if (is_bool($_GET["numberLocationRandom"])) {
-		$numberLocationRandom = $_GET["numberLocationRandom"];
-	}
-	
 	
 	$symbols = $defaultSymbols;
-	if ($_GET["symbols"] > 0 && $_GET["symbols"] <= $maxSymbols) {
-		$symbols = $_GET["symbols"];
-	}
-	
 	$symbolLocationRandom = $defaultSymbolLocationRandom;
-	if (is_bool($_GET["symbolLocationRandom"])) {
-		$symbolLocationRandom = $_GET["symbolLocationRandom"];
-	}
 	
+	// Assume that is "wordCount" is present that the entire form has been submitted.
+	// If a user violates this assumption by modifying the URL, the only ramification
+	// if that notices about the missing indexes will be thrown.
+	if (isset($_GET["wordCount"])) { 
+		if ($_GET["wordCount"] > 0 && $_GET["wordCount"] <= $maxWordCount) {
+			$wordCount = $_GET["wordCount"];
+		}
+		if (in_array($_GET["separator"], $possibleSeparators)) {
+			$separator = $_GET["separator"];
+		}
+		
+		if ($_GET["numbers"] > 0 && $_GET["numbers"] <= $maxNumbers) {
+			$numbers = $_GET["numbers"];
+		}
+		if (is_bool($_GET["numberLocationRandom"])) {
+			$numberLocationRandom = $_GET["numberLocationRandom"];
+		}
+		
+		if ($_GET["symbols"] > 0 && $_GET["symbols"] <= $maxSymbols) {
+			$symbols = $_GET["symbols"];
+		}
+		if (is_bool($_GET["symbolLocationRandom"])) {
+			$symbolLocationRandom = $_GET["symbolLocationRandom"];
+		}
+	}
 	
 	//Generate Word List
 	$wordList = array("Aardvark", "Beetle", "Cicada", "Dolphin", "Elk", "Fire", "Goad", "Hammer", "Ice", "Jug");
@@ -66,8 +68,20 @@
 		$password = $password . $wordList[$index];
 		
 		// Append separator to all but the last word
+		// else add numbers and symbols to the end of the word
 		if ($i < $wordCount - 1) {
-				$password = $password . $separator;
+			$password = $password . $separator;
+		} else {
+			if ($symbols > 0) {
+				for ($j = 0; $j < $symbols; $j++) {
+					$password = $password . chr(rand(33,47));
+				}
+			}
+			if ($numbers > 0) {
+				for ($j = 0; $j < $numbers; $j++) {
+					$password = $password . rand(0,9);
+				}
+			}
 		}
 	}
 	
